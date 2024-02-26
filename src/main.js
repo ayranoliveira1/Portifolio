085a63d835adea4b265dba805a52f33584ceadb9
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-26 17:20:32 -0300

    Feat: adicionar sessão de conhecimentos na pagina

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { lista } from "./dados.js";
+import { lista, projetos } from "./dados.js";
 
 const carregarLinguagens = () => {
    for (const linguagensLista of lista) {
@@ -17,3 +17,47 @@ const carregarLinguagens = () => {
 };
 
 carregarLinguagens();
+
+const carregarProjetos = () => {
+   for (const projetosLista of projetos) {
+      const cartaoProjetos = `
+      <div class="projeto wow fadeInUp delay-02s">
+                     <img
+                        class="imgprojeto"
+                        src="/public/assets/${projetosLista.img}"
+                        alt=""
+                     />
+
+                     <div class="projetos2">
+                        <h2>${projetosLista.name}</h2>
+                        <p>
+                           ${projetosLista.sobre}
+                        </p>
+
+                        <div class="tech">
+                           <p>${projetosLista.tech}</p>
+                        </div>
+                        <div class="projetosb">
+                           <a href="${projetosLista.github}"
+                              target="_blank"
+                              ><button class="button-projetos">
+                                 GitHub
+                              </button></a
+                           >
+                           <a href="${projetosLista.deploy}"
+                              target="_blank"
+                              ><button class="button-projetos">
+                                 Deploy
+                           </button></a
+                           >
+                        </div>
+                     </div>
+                  </div>
+               </div>
+      `;
+
+      document.querySelector("#projetosL").innerHTML += cartaoProjetos;
+   }
+};
+
+carregarProjetos();
